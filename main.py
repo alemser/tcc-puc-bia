307ad709d768bfb1e12ba49e6786d647f20ad7de
Alessandro Lemser <lemser.alessandro@gmail.com>
2021-07-28 13:01:48 +0100

    Fix: cleanup

--- main.py
+++ main.py
@@ -13,27 +13,27 @@ def main():
     threads.append(t1)
     time.sleep(10)
 
-    t2 = Thread(get_urls, ('wedding', 999))
+    t2 = Thread(target=get_urls, args=('wedding', 999))
     t2.start()
     threads.append(t2)
     time.sleep(10)
 
-    t3 = Thread(get_urls, ('nature', 999))
+    t3 = Thread(target=get_urls, args=('nature', 999))
     t3.start()
     threads.append(t3)
     time.sleep(10)
 
-    t4 = Thread(get_urls, ('widelife', 999))
+    t4 = Thread(target=get_urls, args=('widelife', 999))
     t4.start()
     threads.append(t4)
     time.sleep(10)
 
-    t5 = Thread(get_urls, ('landscape', 999))
+    t5 = Thread(target=get_urls, args=('landscape', 999))
     t5.start()
     threads.append(t5)
     time.sleep(10)
 
-    t6 = Thread(get_urls, ('portrait', 999))
+    t6 = Thread(target=get_urls, args=('portrait', 999))
     t5.start()
     threads.append(t5)
     time.sleep(10)
