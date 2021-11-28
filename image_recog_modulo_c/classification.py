d7cdd50bb86d631a76e276286eae8ed09cde9a4c
Alessandro Lemser <lemser.alessandro@gmail.com>
2021-11-28 11:40:11 +0000

    Adicionando id_categoria_ml e queries

--- image_recog_modulo_c/classification.py
+++ image_recog_modulo_c/classification.py
@@ -7,22 +7,42 @@ from time import sleep
 import tensorflow.keras
 from keras.preprocessing import image
 import tensorflow as tf
+import psycopg2
+import urllib.request
+
+DB_HOST = 'localhost'
+DB_PORT = 5432
+DB_USER = 'alemser'
+DB_PASS = ''
+DB_NAME = 'postgres'
 
 model = tensorflow.keras.models.load_model("keras_model.h5")
 
+categorias_existentes = {}
 
-image = cv2.imread("img_sport.jpg")
-img = cv2.resize(image,(224,224))
-img = np.array(img,dtype=np.float32)
-img = np.expand_dims(img,axis=0)
+with psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT) as conn:
+    with conn.cursor() as cur:
+        cur.execute("""SELECT id_categoria, nm_categoria FROM d_categoria""")
+        for record in cur:
+            categorias_existentes[record[1]] = record[0]
 
-img = img/255
+        cur.execute("""SELECT id_imagem, nm_url FROM d_imagem limit 10""")
+        for record in cur:
+            req = urllib.request.urlopen(record[1])
+            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+            image = cv2.imdecode(arr, -1)
 
-prediction = model.predict(img)
-predicted_class = np.argmax(prediction[0], axis=-1)
+            img = cv2.resize(image,(224,224))
+            img = np.array(img,dtype=np.float32)
+            img = np.expand_dims(img,axis=0)
 
-if 0 == predicted_class:
-    print("Natureza")
+            img = img/255
 
-if 1 == predicted_class:
-    print("Esporte")
+            prediction = model.predict(img)
+            predicted_class = np.argmax(prediction[0], axis=-1)
+
+            if 0 == predicted_class:
+                print("Natureza", "Codigo", categorias_existentes["Natureza"])
+
+            if 1 == predicted_class:
+                print("Esporte", "Codigo", categorias_existentes["Esporte"])
