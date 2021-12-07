2e994652896c775a82f5d4c847b454dde9ec9453
Alessandro Lemser <lemser.alessandro@gmail.com>
2021-12-07 10:37:29 +0000

    Adicionando comentarios no script

--- image_recog_modulo_c/classificacao.py
+++ image_recog_modulo_c/classificacao.py
@@ -10,6 +10,8 @@ import tensorflow as tf
 import psycopg2
 import urllib.request
 
+## Requisitos para execucao: requirements.txt
+
 DB_HOST = "localhost"
 DB_PORT = 5432
 DB_USER = "alemser"
@@ -17,20 +19,25 @@ DB_PASS = ""
 DB_NAME = "postgres"
 LABEL_FNAME = "labels.txt";
 
+# Carregamento do modelo gerado usando o Teachable Machine
 model = tf.keras.models.load_model("keras_model.h5")
-print("Modelo pronto")
 
-categorias_existentes = {}
+# Criado array com as categorias existentes
+categorias_existentes = ["Natureza", "Esporte", "Casamento", "Retrato"]
+
+# Array que ira conter o codigo que cada categoria possui no banco de dados (chave primaria)
+codigo_categorias_no_bd = {}
 
 with psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT) as conn:
     with conn.cursor() as cur:
         conn.autocommit = True
+
+        # Obtendo o codigo de cada categoria (chave primaria)
         cur.execute("""SELECT id_categoria, nm_categoria FROM d_categoria""")
         for record in cur:
-            categorias_existentes[record[1]] = record[0]
+            codigo_categorias_no_bd[record[1]] = record[0]
 
-        print("Mapa de categorias pronto")
-
+        # Execucao principal - obtendo a URL das imagens do banco de dados
         count = 0
         cur.execute(
             """SELECT i.id_imagem, i.nm_url
@@ -38,6 +45,8 @@ with psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HO
                JOIN f_venda f ON f.id_imagem = i.id_imagem""")
         for record in cur:
             try:
+
+                # Leitura e processamento da imagem
                 req = urllib.request.urlopen(record[1])
                 arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
                 image = cv2.imdecode(arr, -1)
@@ -48,25 +57,27 @@ with psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HO
 
                 img = img/255
 
+                # Obtendo as predicoes a partir do modelo
                 prediction = model.predict(img)
+
+                # Obtendo a classe - inteiro que ira corresponder ao indice da categoria em categorias_existentes
                 predicted_class = np.argmax(prediction[0], axis=-1)
+
+                # Obtendo a probabilidade
                 max_prob = int(max(prediction[0]) * 100)
 
-                id_categoria = 0
-                if 0 == predicted_class:
-                    id_categoria = categorias_existentes["Natureza"]
-                if 1 == predicted_class:
-                    id_categoria = categorias_existentes["Esporte"]
-                if 2 == predicted_class:
-                    id_categoria = categorias_existentes["Casamento"]
-                if 3 == predicted_class:
-                    id_categoria = categorias_existentes["Retrato"]
+                # Nome da categoria resultante da classificacao
+                categoria_classificada = categorias_existentes[predicted_class]
 
+                # Obtendo o ID (chave primaria) que a categoria possui no banco de dados
+                id_categoria = codigo_categorias_no_bd[categoria_classificada]
+
+                # Atualizando a dimensao d_imagens com a categoria classificada e a probabilidade
                 insert_cursor = conn.cursor()
                 insert_cursor.execute("""Update d_imagem Set id_categoria_ml = %s, nu_categoria_ml_prob = %s Where id_imagem = %s""", (id_categoria, max_prob, record[0]))
                 conn.commit()
                 count = count + 1
-                print(count, "registros processados")
+                print(count, "registros processados.", categoria_classificada, id_categoria)
             except Exception as e:
                 print("Error:", e)
 
--- image_recog_modulo_c/classificacao_controle.py
+++ image_recog_modulo_c/classificacao_controle.py
@@ -11,18 +11,29 @@ import psycopg2
 import urllib.request
 import os
 
+## Requisitos para execucao: requirements.txt
+
+# Identificando o diretorio onde as imagens de controle estao
 controle_dir = os.getcwd() + "/training_data/controle/"
+
+# Removendo arquivos oriundos de classificacoes anteriores.
 for f in os.listdir(controle_dir):
     if f.endswith(".miss"):
         os.remove(os.path.join(controle_dir, f))
 
+# Carregamento do modelo gerado usando o Teachable Machine
 model = tf.keras.models.load_model("keras_model.h5")
 
+# Criado array com as categorias existentes
 categorias_existentes = ["Natureza", "Esporte", "Casamento", "Retrato"]
 
 for categ in categorias_existentes:
     for i in range(1, 11):
+        # Para cada uma das 10 imagens de cada categoria, realizada a classificacao
+
         try:
+
+            # Leitura e processamento da imagem
             req = urllib.request.urlopen("file://" + controle_dir + categ + str(i) + ".jpg")
             arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
             image = cv2.imdecode(arr, -1)
@@ -33,16 +44,18 @@ for categ in categorias_existentes:
 
             img = img/255
 
+            # Obtendo as predicoes a partir do modelo
             prediction = model.predict(img)
+
+            # Obtendo a classe - inteiro que ira corresponder ao indice da categoria em categorias_existentes
             predicted_class = np.argmax(prediction[0], axis=-1)
-            max_prob = int(max(prediction[0]) * 100)
 
             if categ != categorias_existentes[predicted_class]:
+                # Modelo classificou de forma diferente - registrando isso no arquivo .miss
                 print(categ, " difere de ", categorias_existentes[predicted_class])
                 f = open(controle_dir + categ + str(i) + ".miss", "a")
                 f.write("Classificado pelo modelo como: " + categorias_existentes[predicted_class])
                 f.close()
-
         except Exception as e:
             print("Error:", e)
 
